b47bedf9f1821136114285fc56370f7e4dc0365d
Joe Ng'ethe <joeynimu@gmail.com>
2015-07-06 10:24:05 +0300

    Update index.php

--- index.php
+++ index.php
@@ -11,18 +11,34 @@ $jsonData = file_get_contents($fileURL);
 <head>
 <meta charset="utf-8">
 <title>Sample Intelligent Search</title>
-<link rel="stylesheet" href="assets/css/style.css"/>	
+<link rel="stylesheet" href="assets/css/style.css"/>  
 <link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
   <script src="assets/js/jquery-1.10.2.js"></script>
   <script src="assets/js/jquery-ui.js"></script>
   <script src="assets/js/storage.js"></script>
+  <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" />
 
 </head>
  <body>
- <div class="pageContent">
-  <div class="searchBar">
-  	<input type="text" id="searchField" style="width: 70%;" name="searchTerm" value="" required placeholder="search here..."/>
-  	<input type="button" id="searchBtn" style="width: 20%;" name="submit" value="Search" onclick="searchUsers()"/>
+  </head>
+ <body>
+   <header>
+      <nav class="navbar navbar-default">
+      <div class="container">
+        <div class="navbar-header">
+          <a class="navbar-brand" href="index.php">Simple Search Engine</a>
+        </div>
+
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="index.php"><i class="fa fa-home"></i> Home</a></li>
+        </ul>
+      </div>
+      </nav>
+    </header>
+ <div class="container">
+  <div class="form-group">
+    <input type="text" id="searchField" style="width: 70%;" name="searchTerm" value="" class="form-control" required placeholder="search here..."/>
+    <input type="button" id="searchBtn" style="width: 20%;" name="submit" value="Search" class="btn btn-primary" onclick="searchUsers()"/>
   </div>
   </br>
   <div class="generalWrapper">
@@ -31,20 +47,32 @@ $jsonData = file_get_contents($fileURL);
    <!-- loop through data and display profiles -->
     <?php      
       $jsonParsed=json_decode($jsonData, true);
+      
+
       //get total users
-      $totalUsers = $jsonParsed[total];
+      //HOWEVER< THE PEOPLE JSON FILE HAS DATA THAT IS TRUNCATED AND I HAD TO REPAIR IT A BIT
+      //HENCE, we are going to hard code the total users to match what is on the file
+      // $totalUsers = $jsonParsed['total'];
+
+      $totalUsers = 103;
+
       //get the result set
-      $resultSet =  $jsonParsed[result];
+      $resultSet =  $jsonParsed['result'];
 
       //loop and get data; using a few fields here
       for ($i=0; $i < $totalUsers; $i++) { 
-      	 $oneUserId = $resultSet[$i][id];
-         $oneUserPic = $resultSet[$i][picture];
-         $oneUserName = $resultSet[$i][name];
-         $oneUserCompany = $resultSet[$i][company];
-         $oneUserPhone = $resultSet[$i][phone];
+        
+         $oneUserObject = $resultSet[$i];
 
-        if($oneUserId!='')
+         if(isset($oneUserObject) && $oneUserObject!='')
+         {
+         $oneUserId = $oneUserObject['id'];
+         $oneUserPic = $oneUserObject['picture'];
+         $oneUserName = $oneUserObject['name'];
+         $oneUserCompany = $oneUserObject['company'];
+         $oneUserPhone = $oneUserObject['phone'];
+
+        if(isset($oneUserId) && $oneUserId!='')
          {
          ?>
         <a href="singleView.php?id=<?php echo $oneUserId; ?>" class="noHighlight">
@@ -64,6 +92,7 @@ $jsonData = file_get_contents($fileURL);
         </div>
         </a>
          <?php
+         }
         }
       }    
     ?>     
@@ -93,25 +122,25 @@ $jsonData = file_get_contents($fileURL);
            // var dataToParse = JSON.parse(s);
 
            var countSuggestions = 0, suggestionsString = "";
-           for (var i =0; i < dataToParse.length; i++) {           	               	   
-           	   if(countSuggestions<3)
-           	   {
-           	      singleGender = dataToParse[i].gender;
-           	      //check  if this user's attributes are part of our suggestion criteria
-           	      if(userAttributes.indexOf(singleGender) >-1)
-           	      { 
-           	      	countSuggestions+=1;
-           	      	//show this user
+           for (var i =0; i < dataToParse.length; i++) {                               
+               if(countSuggestions<3)
+               {
+                  singleGender = dataToParse[i].gender;
+                  //check  if this user's attributes are part of our suggestion criteria
+                  if(typeof singleGender != 'undefined' && userAttributes.indexOf(singleGender) >-1)
+                  { 
+                    countSuggestions+=1;
+                    //show this user
                       console.log("TO SUGGEST: "+dataToParse[i].name);
                       suggestionsString = suggestionsString+'<br>Based on gender clicked earlier <br><a href="singleView.php?id='+dataToParse[i].id+'">'+dataToParse[i].name+'</a>';
-           	      }
-           	  }
-           	  else
-           	  {
-           	  	//enough suggestions; break loop
-           	  	console.log("TO BREAK LOOP: ");
-           	  	break;
-           	  }
+                  }
+              }
+              else
+              {
+                //enough suggestions; break loop
+                console.log("TO BREAK LOOP: ");
+                break;
+              }
            }
 
           $("#suggestionsZone").html("People you may know: <br><br> "+suggestionsString);
@@ -134,24 +163,24 @@ $jsonData = file_get_contents($fileURL);
 
 //method to search for users from file
   function searchUsers()
-  {  	
-  	var searchValue = $("#searchField").val();
+  {   
+    var searchValue = $("#searchField").val();
 
-  	if(searchValue!=='')
-  	{
-  	//get earlier JSON
+    if(searchValue!=='')
+    {
+    //get earlier JSON
     var searchableJSON = <?php echo $jsonData; ?>;
 
-  	console.log("To search via AJAX with: "+searchValue+" from: "+searchableJSON);
+    console.log("To search via AJAX with: "+searchValue+" from: "+searchableJSON);
 
-  	//save the keyword for future reference
+    //save the keyword for future reference
     updateUserKeywords(searchValue);
 
-  	//get objects where the name matches the user search term
-  	//search and get objects
-  	matchingObjects = [];
+    //get objects where the name matches the user search term
+    //search and get objects
+    matchingObjects = [];
 
-  	var searchResponse = JSON.stringify(getObjects(searchableJSON,'name',searchValue));
+    var searchResponse = JSON.stringify(getObjects(searchableJSON,'name',searchValue));
 
     console.log("RESULT: "+searchResponse);
 
@@ -176,7 +205,7 @@ $jsonData = file_get_contents($fileURL);
    }
    else
    {
-   	//prompt user to enter search term
+    //prompt user to enter search term
      $("#searchField").attr("placeholder", "Please enter a name");
    }
   }
@@ -186,21 +215,23 @@ function getObjects(obj, key, val) {
     var objects = [];
     for (var i in obj) {
 
-    	var oneObj = obj[i];
+      var oneObj = obj[i];
         if (!obj.hasOwnProperty(i)) continue;
         if (typeof oneObj == 'object') {
             objects = objects.concat(getObjects(oneObj, key, val));    
         } else 
         //if key matches and value matches or if key matches and value is not passed (eliminating the case where key matches but passed value does not)
-        var comparator = JSON.stringify(oneObj);
+        comparator = JSON.stringify(oneObj);
          // console.log("In search: "+comparator);
+         //convert comparator to upper case to avoid null where letter cases differ
+         comparator = comparator.toUpperCase();
 
        //check for undefined value
        if(typeof comparator != 'undefined')
        {
-        if (i == key && comparator.indexOf(val) >-1 || i == key && val == '') { //
+        if (i == key && comparator.indexOf(val.toUpperCase()) >-1 || i == key && val == '') { //
             objects.push(obj);
-        } else if (comparator.indexOf(val) >-1 && key == ''){
+        } else if (comparator.indexOf(val.toUpperCase()) >-1 && key == ''){
             //only add if the object is not already in the array
             if (objects.lastIndexOf(obj) == -1){
                 objects.push(obj);
